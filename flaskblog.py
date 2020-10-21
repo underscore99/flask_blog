28faded9ba29bede82322d92f50c446a6cafe856
underscore99 <72135845+underscore99@users.noreply.github.com>
2020-10-21 13:29:40 +0100

    i dont know what im doing

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
   
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5afd723422802a8cedff0e0ff00d8976'
 
 posts = [
     {
@@ -28,6 +31,15 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/login")
+def login():
+    form = LoginForm
+    return render_template('login.html', title='login', form=form)
+
+@app.route("/register")
+def register():
+    form = RegistrationForm
+    return render_template('register.html', title='register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
